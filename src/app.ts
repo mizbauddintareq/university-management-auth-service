135f0a1acd1bf3bcbfa7458aad26b094e15c0d6b
mizbauddintareq <mizbauddintareq@gmail.com>
2023-05-29 09:25:35 +0600

    setup eslint prettier huskey lint stage

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 // Testing
-app.get('/', (req: Request, res: any) => {
+app.get('/', (req: Request, res: ) => {
   res.send('working successfully')
 })
 
