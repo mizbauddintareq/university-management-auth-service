c03b449859eb2330b465b56586f80c6e0610a2ac
mizbauddintareq <mizbauddintareq@gmail.com>
2023-05-29 10:04:26 +0600

    Create user interface and model

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import cors from 'cors'
-import express, { Application, Request } from 'express'
+import express, { Application, Request, Response } from 'express'
 const app: Application = express()
 
 // using cors
@@ -10,7 +10,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 // Testing
-app.get('/', (req: Request, res: any) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('working successfully')
 })
 
